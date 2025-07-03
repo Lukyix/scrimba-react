dcb062cb645f505479f54072fdd2b05782fa4ef0
Zelo <lukyy6329@gmail.com>
2025-07-03 19:11:57 +0200

    useState

---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img src="/src/images/chef-claude-icon.png" alt="Chef claude" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
